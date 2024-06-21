d712e6d7e5fc4f4a1a29aefcb9499cbbd5ef361f
Menna Ashraf <menna.ashraf@holouly.co>
2024-06-21 21:54:50 +0300

    basic operations file

---
+++ pythonProject/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+adittion=number1+number2
+subtraction=number1-number2
+multiplication=number1*number2
+print("Addition of " + str(number1) +" and " + str(number2) + " is " + str(adittion) )
+print("Subtraction of " + str(number1) +" and " + str(number2) + " is " + str(subtraction) )
+print("Multiplication of " + str(number1) +" and " + str(number2) + " is " + str(multiplication) )
