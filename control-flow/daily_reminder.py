fb26146d4e340d09cc4fe006d9b227cf5bf23c26
Menna Ashraf <menna.ashraf@holouly.co>
2024-07-05 22:57:04 +0300

    add match_case_calculator.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,11 +3,11 @@ priority = str(input("Priority (high/medium/low):")).lower()
 time_bound = str(input("Is it time-bound? (yes/no):")).lower()
 match priority:
     case "high":
-        reminder = f"Reminder: '{task}' is a high priority task"
+        reminder = f"'{task}' is a high priority task"
     case "medium":
-        reminder = f"Reminder: '{task}' is a medium priority task"
+        reminder = f"'{task}' is a medium priority task"
     case "low":
-        reminder = f"Note '{task}' is a low priority task"
+        reminder = f"'{task}' is a low priority task"
     case _:
         reminder = f"'{task}' has an undefined priority level"
 if time_bound == "yes":
@@ -15,4 +15,4 @@ if time_bound == "yes":
 else:
     reminder += ". Consider completing it when you have free time."
 # Print the reminder
-print(f"{reminder}")
+print(f"Reminder: {reminder}")
