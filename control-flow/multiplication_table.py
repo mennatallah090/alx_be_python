7708a9c86ed2f1c732e42daec3acf283726249e8
Menna Ashraf <menna.ashraf@holouly.co>
2024-07-05 22:02:40 +0300

    add match_case_calculator.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,3 @@
-num = int(input("Enter a number to see its multiplication table:"))
-for item in range(1, 11):
-    print(f"{num}*{item}={item * num}")
+number = int(input("Enter a number to see its multiplication table:"))
+for i in range(1, 11):
+    print(f"{number} * {i} = {number * i}")
