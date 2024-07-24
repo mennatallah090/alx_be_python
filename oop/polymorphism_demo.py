30b79968c77ecf39fb6920764c5ab08411fc4875
Menna Ashraf <menna.ashraf@holouly.co>
2024-07-24 14:35:01 +0300

    book class

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,27 @@
+from math import pi
+
+
+class Shape:
+    def __init__(self):
+        pass
+
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+    def area(self):
+        return pi * self.radius ** 2
